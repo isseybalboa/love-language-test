73201632e9bce6a2ff3822955f17193a372067af
isseybalboa <62413001+isseybalboa@users.noreply.github.com>
2026-03-05 14:04:39 +0900

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+{
+q:"恋人にしてもらうと嬉しいのは？",
+a:{text:"プレゼント",type:"gift"},
+b:{text:"一緒に過ごす時間",type:"time"}
+},
+{
+q:"嬉しいと感じるのは？",
+a:{text:"ありがとうと言われる",type:"word"},
+b:{text:"ハグされる",type:"touch"}
+}
+];
